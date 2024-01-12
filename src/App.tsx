7b2e4631ede35cd95e028575eb24d8db3230294b
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-12 13:50:36 +0530

    feat: app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "pages/Home";
+import VideoInsights from "pages/VideoInsights";
 
 const App = () => {
-  return <div className="flex justify-center items-center">APP</div>;
+  return (
+    <>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/insights" element={<VideoInsights />} />
+      </Routes>
+    </>
+  );
 };
 
 export default React.memo(App);
