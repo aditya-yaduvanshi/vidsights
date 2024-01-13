d0cd8b7f205aed329937d59de7b70ff7e4c37651
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-13 18:57:14 +0530

    feat: proceed to answers step

--- src/pages/VideoInsights.tsx
+++ src/pages/VideoInsights.tsx
@@ -3,6 +3,7 @@ import QuestionsStep from "components/QuestionsStep";
 import SelectVideos from "components/SelectVideos";
 import React from "react";
 import { useSearchParams } from "react-router-dom";
+import getExtractedQuestions from "utils/getExtractedQuestions";
 
 const VideoInsights = () => {
   const [params, setParams] = useSearchParams();
@@ -12,7 +13,24 @@ const VideoInsights = () => {
     <>
       <main className="p-5 h-full w-full">
         {step === "answers" && <AnswersStep />}
-        {step === "questions" && <QuestionsStep />}
+        {step === "questions" && (
+          <QuestionsStep
+            onNext={() =>
+              setParams((prev) => {
+                const searchParams = new URLSearchParams(prev.toString());
+                const questions = searchParams.get("questions") || "";
+                const qs = getExtractedQuestions(questions);
+                if (!qs.length) {
+                  alert("No questions found. Please provide questions.");
+                } else {
+                  searchParams.set("step", "answers");
+                  searchParams.set("questions", qs.join(" "));
+                }
+                return searchParams;
+              })
+            }
+          />
+        )}
         {(!step || step === "selectVideos") && (
           <SelectVideos
             onNext={() =>
